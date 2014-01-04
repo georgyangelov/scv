948fae709cae93dc62e431ba9c96e376fcaaa9a7
Georgy Angelov <georgyangelov@gmail.com>
2014-01-04 20:44:06 +0200

    SCV can now create commits!

--- lib/scv/objects/commit.rb
+++ lib/scv/objects/commit.rb
@@ -4,6 +4,14 @@ module SCV
   module Objects
 
     class Commit < VCSToolkit::Objects::Commit
+
+      def initialize(date:, **kwargs)
+        # Parse the date on deserialization
+        date = DateTime.parse date if date.is_a? String
+
+        super
+      end
+
     end
 
   end
--- lib/scv/repository.rb
+++ lib/scv/repository.rb
@@ -1,37 +1,50 @@
-require 'forwardable'
 require 'vcs_toolkit/repository'
 
 module SCV
-  class Repository
-    def initialize(path, init: true)
+  class Repository < VCSToolkit::Repository
+    def initialize(path, init: false)
       repository_path = "#{path}/.scv"
 
       working_directory = FileStore.new   path
       object_store      = ObjectStore.new repository_path
 
-      @vcs_repository = VCSToolkit::Repository.new object_store,
-                                                   working_directory,
-                                                   commit_class: Objects::Commit,
-                                                   tree_class:   Objects::Tree,
-                                                   blob_class:   Objects::Blob,
-                                                   label_class:  Objects::Label
+      super object_store,
+            working_directory,
+            commit_class: Objects::Commit,
+            tree_class:   Objects::Tree,
+            blob_class:   Objects::Blob,
+            label_class:  Objects::Label
 
-      create_label :head, nil if init
+      set_label :head, nil if init
 
       self.head = get_object(:head)
     end
 
-    extend Forwardable
-    def_delegators :@vcs_repository, :head, :head=
+    ##
+    # Sets the current head commit and saves it. See VCSToolkit::Repository#head=
+    #
+    def head=(commit_or_label_or_object_id)
+      super
+
+      set_label :head, head
+    end
 
     ##
     # Return the object with this object_id or nil if it doesn't exist.
     #
     def get_object(object_id)
-      @vcs_repository.repository.fetch object_id if @vcs_repository.repository.key? object_id
+      repository.fetch object_id if repository.key? object_id
     end
 
     alias_method :[], :get_object
+
+    ##
+    # Creates a commit which is a snapshot of the
+    # staging area (currently the working directory).
+    #
+    def commit(message, author, date)
+      super message, author, date, ignores: [/^\./]
+    end
 
     ##
     # Create an empty repository in the specified directory.
@@ -55,10 +68,10 @@ module SCV
     #
     # If the label already exists it is overriden.
     #
-    def create_label(name, reference_id)
+    def set_label(name, reference_id)
       label = Objects::Label.new object_id: name, reference_id: reference_id
 
-      @vcs_repository.repository.store name, label
+      repository.store name, label
     end
   end
 end
