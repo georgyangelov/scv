94e34b525c1f280a1dbbaee884c8df96be3f30c4
Georgy Angelov <georgyangelov@gmail.com>
2014-01-23 16:26:21 +0200

    Support --source/-s option for `scv reset`
    Sets the source commit of the reset. It can be given
    a label or a commit id.
    
    It can also be of the form '#{object_id}~#{num}' where num is
    the commit offset (the number of times that its parent should be
    followed).
    For example 'head~3' means the fourth commit before the current one.
    'head~0' is the same as 'head'.

--- bin/commands/reset.rb
+++ bin/commands/reset.rb
@@ -1,11 +1,17 @@
-desc 'Resets files to the state they were in the current commit'
+desc 'Resets files to the state they were in the specified commit'
 arg_name '<file> ...'
 command [:reset, :restore] do |c|
+  c.desc          'Use this specify which commit should be the used as reference'
+  c.arg_name      'source'
+  c.default_value 'head'
+  c.flag          [:s, :source]
+
   c.action do |global_options, options, args|
     repository = SCV::Repository.new global_options[:dir]
+    commit     = repository.resolve options[:source], :commit
 
     args.each do |file_path|
-      repository.reset_file file_path, repository.head
+      repository.reset_file file_path, commit.id
     end
   end
 end
