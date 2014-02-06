150d6ff4d6d9a8728a4cc0e628c00921a3e90e73
Georgy Angelov <georgyangelov@gmail.com>
2014-02-06 23:49:56 +0200

    Status information after the merge

--- bin/commands/merge.rb
+++ bin/commands/merge.rb
@@ -7,10 +7,33 @@ command :merge do |c|
 
     raise 'No branch specified to merge from' if args.empty?
 
-    commit_two = repository.resolve(args[0], :commit)
+    commit_two = repository.resolve(args.first, :commit)
 
     raise 'There is no branch with that name' if commit_two.nil?
 
-    repository.merge commit, commit_two
+    merge_status = repository.merge commit, commit_two
+
+    puts "# Merged #{args[0]} into #{repository.head}"
+    puts
+
+    if merge_status[:merged].any?
+      puts "# Automatic merges:"
+
+      merge_status[:merged].each do |file|
+        puts "    #{file}".yellow
+      end
+
+      puts
+    end
+
+    if merge_status[:conflicted].any?
+      puts "# Conflicted files:"
+
+      merge_status[:conflicted].each do |file|
+        puts "    #{file}".red
+      end
+
+      puts
+    end
   end
 end
