8a0e56a24e7cf4f0bdeea3278836b302c22bb9ee
Georgy Angelov <georgyangelov@gmail.com>
2014-02-06 23:43:58 +0200

    Merge: Add basic error checks

--- bin/commands/merge.rb
+++ bin/commands/merge.rb
@@ -1,10 +1,15 @@
 desc 'Merges two branches to the working directory'
-arg_name ''
+arg_name '<branch name>'
 command :merge do |c|
   c.action do |global_options, options, args|
     repository = global_options[:repository]
     commit     = repository.resolve(:head, :commit)
+
+    raise 'No branch specified to merge from' if args.empty?
+
     commit_two = repository.resolve(args[0], :commit)
+
+    raise 'There is no branch with that name' if commit_two.nil?
 
     repository.merge commit, commit_two
   end
